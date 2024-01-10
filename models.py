48b2f044e694c7e8730ca1a83e5757e35855eaaa
connor <caschm@gmail.com>
2024-01-10 10:23:13 -0500

    create user model

--- models.py
+++ models.py
@@ -1,1 +1,34 @@
-"""Models for Blogly."""
+"""Models for the Blogle app"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(50),  # string with max len of 50 chars
+                     nullable=False,
+                     unique=True)
+    
+    last_name = db.Column(db.String(50),  # string with max len of 50 chars
+                     nullable=False,
+                     unique=True)
+
+    image_url = db.Column(db.String(200), nullable=False, default="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg")
+
+    def __repr__(self):
+        p = self
+        return f"<User id={p.id} first_name={p.first_name} last_name={p.last_name}"
+
+
+                     
+
+
